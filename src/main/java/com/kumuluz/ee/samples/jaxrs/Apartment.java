9972254a036d6d12352bdd709db2a774b28ec5a9
MatejDolenc <dolencmatej3@gmail.com>
2017-10-22 11:35:23 +0200

    First commit

---
+++ src/main/java/com/kumuluz/ee/samples/jaxrs/Apartment.java
@@ -0,0 +1,36 @@
+
+package com.kumuluz.ee.samples.jaxrs;
+
+/**
+ * @author Matej Dolenc
+ */
+public class Apartment {
+
+    private String id;
+    private String numOfBeds;
+    private String customerId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNumOfBeds() {
+        return numOfBeds;
+    }
+
+    public void setNumOfBeds(String numOfBeds) {
+        this.numOfBeds = numOfBeds;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+}
---
+++ src/main/java/com/kumuluz/ee/samples/jaxrs/CustomerResource.java
@@ -0,0 +1,113 @@
+/*
+ *  Copyright (c) 2014-2017 Kumuluz and/or its affiliates
+ *  and other contributors as indicated by the @author tags and
+ *  the contributor list.
+ *
+ *  Licensed under the MIT License (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  https://opensource.org/licenses/MIT
+ *
+ *  The software is provided "AS IS", WITHOUT WARRANTY OF ANY KIND, express or
+ *  implied, including but not limited to the warranties of merchantability,
+ *  fitness for a particular purpose and noninfringement. in no event shall the
+ *  authors or copyright holders be liable for any claim, damages or other
+ *  liability, whether in an action of contract, tort or otherwise, arising from,
+ *  out of or in connection with the software or the use or other dealings in the
+ *  software. See the License for the specific language governing permissions and
+ *  limitations under the License.
+*/
+package com.kumuluz.ee.samples.jaxrs;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.util.EntityUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.ws.rs.InternalServerErrorException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * @author Benjamin Kastelic
+ * @since 2.3.0
+ */
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@Path("customers")
+public class CustomerResource {
+
+    private HttpClient httpClient;
+    private ObjectMapper objectMapper;
+
+    @GET
+    public Response getAllCustomers() {
+        List<Customer> customers = Database.getCustomers();
+        return Response.ok(customers).build();
+    }
+
+    @GET
+    @Path("{customerId}")
+    public Response getCustomer(@PathParam("customerId") String customerId) {
+        Customer customer = Database.getCustomer(customerId);
+
+        List<Apartment> apartments = getApartments(customerId);
+
+        customer.setApartments(apartments);
+
+        return customer != null
+                ? Response.ok(customer).build()
+                : Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @POST
+    public Response addNewCustomer(Customer customer) {
+        Database.addCustomer(customer);
+        return Response.noContent().build();
+    }
+
+    @DELETE
+    @Path("{customerId}")
+    public Response deleteCustomer(@PathParam("customerId") String customerId) {
+        Database.deleteCustomer(customerId);
+        return Response.noContent().build();
+    }
+
+    public List<Apartment> getApartments(String customerId){
+        try {
+            //basePath = http://localhost:8080
+            HttpGet request = new HttpGet("http://localhost:8080" + "/v1/apartments/customer/" + customerId);
+            HttpResponse response = httpClient.execute(request);
+
+            int status = response.getStatusLine().getStatusCode();
+
+            if (status >= 200 && status < 300) {
+                HttpEntity entity = response.getEntity();
+
+                if (entity != null)
+                    return getObjects(EntityUtils.toString(entity));
+            } else {
+                String msg = "Remote server '" + "http://localhost:8080" + "' is responded with status " + status + ".";
+                throw new InternalServerErrorException(msg);
+            }
+
+        } catch (IOException e) {
+            String msg = e.getClass().getName() + " occured: " + e.getMessage();
+            throw new InternalServerErrorException(msg);
+        }
+        return new ArrayList<>();
+    }
+
+    private List<Apartment> getObjects(String json) throws IOException {
+        return json == null ? new ArrayList<>() : objectMapper.readValue(json,
+                objectMapper.getTypeFactory().constructCollectionType(List.class, Apartment.class));
+    }
+}
